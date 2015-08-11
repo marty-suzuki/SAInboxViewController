7d46e891b22581dec7552a75130558c52990a1e5
Taiki Suzuki <s1180183@gmail.com>
2015-08-11 16:37:16 +0900

    add sample project

---
+++ SAInboxViewController/SAInboxViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SAInboxViewController.swift
+//  SAInboxViewController
+//
+//  Created by 鈴木大貴 on 2015/08/08.
+//  Copyright (c) 2015年 鈴木大貴. All rights reserved.
+//
+
+import Foundation
---
+++ SAInboxViewControllerSample/SAInboxViewControllerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SAInboxViewControllerSample
+//
+//  Created by 鈴木大貴 on 2015/08/08.
+//  Copyright (c) 2015年 鈴木大貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
